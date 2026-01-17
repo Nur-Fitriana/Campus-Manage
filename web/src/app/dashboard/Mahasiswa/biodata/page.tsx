8e782b80d264ffcd103e75a04848a309c3bff8c1
cindyayu052-hue <cindyayu052@gmail.com>
2026-01-17 07:39:49 +0700

    membuat page biodata

---
+++ web/src/app/dashboard/Mahasiswa/biodata/page.tsx
@@ -0,0 +1,8 @@
+export default function BiodataPage() {
+    return (
+      <div className="p-6 bg-white shadow-sm mt-5 border border-gray-200">
+        <h2 className="text-[#800000] font-bold uppercase text-sm border-b pb-2 mb-4">Profil Lengkap Mahasiswa</h2>
+        {/* Isi dengan tabel data diri */}
+      </div>
+    );
+  }
